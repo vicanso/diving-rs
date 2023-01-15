0b3fa177b7e220102dcba22dc76ad3c47d54f0f3
vicanso <tree.xie@outlook.com>
2023-01-15 20:45:58 +0800

    refactor: adjust get files from layer

--- src/image/layer.rs
+++ src/image/layer.rs
@@ -2,7 +2,7 @@ use bytes::Bytes;
 use libflate::gzip::Decoder;
 use serde::{Deserialize, Serialize};
 use snafu::{ResultExt, Snafu};
-use std::{io::Read, path::Path};
+use std::io::Read;
 
 #[derive(Debug, Snafu)]
 pub enum Error {
@@ -17,9 +17,14 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 #[serde(rename_all = "camelCase")]
 pub struct FileInfo {
     pub path: String,
+    pub link: String,
+    pub size: u64,
+    pub mode: u32,
+    pub uid: u64,
+    pub gid: u64,
 }
 
-pub async fn get_files_from_layer(data: Vec<u8>, is_gzip: bool) -> Result<()> {
+pub async fn get_files_from_layer(data: Vec<u8>, is_gzip: bool) -> Result<Vec<FileInfo>> {
     let mut buf = data;
     if is_gzip {
         let mut decoder = Decoder::new(&buf[..]).context(GzipDecodeSnafu {})?;
@@ -30,11 +35,20 @@ pub async fn get_files_from_layer(data: Vec<u8>, is_gzip: bool) -> Result<()> {
         let data = Bytes::copy_from_slice(&decode_data);
         buf = data.to_vec()
     }
+    let mut files = vec![];
     let mut a = tar::Archive::new(&buf[..]);
     for file in a.entries().context(TarSnafu {})? {
-        // Make sure there wasn't an I/O error
         let file = file.context(TarSnafu {})?;
-        // if let Ok(path) = file.path() {
+        let header = file.header();
+        // 不返回目录
+        if header.entry_type().is_dir() {
+            continue;
+        }
+        let mut link = "".to_string();
+
+        if let Some(value) = file.link_name().context(TarSnafu {})? {
+            link = value.to_string_lossy().to_string()
+        }
 
         let info = FileInfo {
             path: file
@@ -42,19 +56,13 @@ pub async fn get_files_from_layer(data: Vec<u8>, is_gzip: bool) -> Result<()> {
                 .context(TarSnafu {})?
                 .to_string_lossy()
                 .to_string(),
+            link,
+            size: file.size(),
+            mode: header.mode().context(TarSnafu {})?,
+            uid: header.uid().context(TarSnafu {})?,
+            gid: header.gid().context(TarSnafu {})?,
         };
-
-        println!("{:?}", file.link_name());
-
-        // Inspect metadata about the file
-        // println!("{:?}", file.header());
-        println!("{:?}", info);
-
-        // files implement the Read trait
-        // let mut s = String::new();
-        // file.read_to_string(&mut s).unwrap();
-        // println!("{}", s);
-        // }
+        files.push(info);
     }
-    Ok(())
+    Ok(files)
 }
