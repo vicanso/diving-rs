da76ac4f6a2533dfa04f152d74cf9a36062e822c
vicanso <tree.xie@outlook.com>
2023-02-05 21:12:42 +0800

    refactor: adjust file tree view

--- src/ui/files.rs
+++ src/ui/files.rs
@@ -32,8 +32,12 @@ pub struct FilesWidget<'a> {
 }
 
 #[derive(Default, Debug, Clone)]
-struct PathInfo {
-    size: u64,
+struct FileTreeItem {
+    pub permission: String,
+    pub uid_gid: String,
+    pub size: u64,
+    pub name: String,
+    pub dir: String,
 }
 
 pub fn new_files_widget(result: &ImageAnalysisResult, opt: FilesWidgetOption) -> FilesWidget {
@@ -67,60 +71,72 @@ pub fn new_files_widget(result: &ImageAnalysisResult, opt: FilesWidgetOption) ->
     let permission_width = name_list[0].len();
     let id_width = name_list[1].len();
     let size_width = name_list[2].len();
-    let mut path_map: HashMap<String, PathInfo> = HashMap::new();
-    let get_dirs = |path: &str| -> Vec<String> {
-        let arr: Vec<&str> = path.split('/').collect();
-        let mut dirs = vec![];
-        for i in 0..arr.len() - 1 {
-            dirs.push(arr[0..i + 1].join("/"));
-        }
-        dirs
-    };
+    // let mut path_map: HashMap<String, PathInfo> = HashMap::new();
+
     let get_file_mode_str = |mode: &str| -> String {
         mode.pad_to_width_with_alignment(permission_width, pad::Alignment::Middle)
     };
-    let get_id_str = |uid: u64, gid: u64| -> String {
-        format!("{uid}:{gid}").pad_to_width_with_alignment(id_width, pad::Alignment::Right)
-    };
+    let get_id_str =
+        |id: &str| -> String { id.pad_to_width_with_alignment(id_width, pad::Alignment::Right) };
     let get_size_str = |size: u64| -> String {
         ByteSize(size)
             .to_string()
             .pad_to_width_with_alignment(size_width, pad::Alignment::Right)
     };
     if let Some(layer) = result.layers.get(opt.selected_layer) {
+        let mut file_tree_items = vec![];
+        let mut dir_size_map = HashMap::new();
         for file in &layer.info.files {
-            for dir in get_dirs(&file.path).iter() {
-                if let Some(info) = path_map.get_mut(dir) {
-                    info.size += file.size;
+            let arr: Vec<&str> = file.path.split('/').collect();
+            for i in 0..arr.len() {
+                // 名称
+                let name = arr[i].to_string();
+                // 文件
+                if i == arr.len() - 1 {
+                    // 文件信息
+                    file_tree_items.push(FileTreeItem {
+                        permission: file.mode.clone(),
+                        uid_gid: format!("{}:{}", file.uid, file.gid),
+                        size: file.size,
+                        name: name.clone(),
+                        ..Default::default()
+                    })
                 } else {
-                    path_map.insert(dir.clone(), PathInfo { size: file.size });
+                    // 目录完整路径
+                    let dir = arr[0..i + 1].join("/");
+                    if let Some(size) = dir_size_map.get(&dir) {
+                        // 增加该目录下的文件大小
+                        dir_size_map.insert(dir, size + file.size);
+                    } else {
+                        // 如果目录不存在
+                        // 记录目录信息
+                        file_tree_items.push(FileTreeItem {
+                            uid_gid: "0:0".to_string(),
+                            size: 0,
+                            name: name.clone(),
+                            dir: dir.clone(),
+                            ..Default::default()
+                        });
+                        // 记录目录下的文件大小
+                        dir_size_map.insert(dir, file.size);
+                    }
                 }
             }
         }
-        for (index, file) in layer.info.files.iter().enumerate() {
-            let dirs = get_dirs(&file.path);
-            for dir in dirs.iter() {
-                if let Some(info) = path_map.get_mut(dir) {
-                    list.push(ListItem::new(Spans::from(vec![
-                        Span::from(get_file_mode_str("-")),
-                        space_span.clone(),
-                        Span::from(get_id_str(0, 0)),
-                        space_span.clone(),
-                        Span::from(get_size_str(info.size)),
-                        space_span.clone(),
-                        Span::from(dir.clone()),
-                    ])));
-                    path_map.remove(dir);
-                }
+
+        for item in file_tree_items.iter() {
+            let mut size = item.size;
+            if !item.dir.is_empty() {
+                size = *dir_size_map.get(&item.dir).unwrap_or(&0);
             }
             list.push(ListItem::new(Spans::from(vec![
-                Span::from(get_file_mode_str(&file.mode)),
+                Span::from(get_file_mode_str(&item.permission)),
                 space_span.clone(),
-                Span::from(get_id_str(file.uid, file.gid)),
+                Span::from(get_id_str(&item.uid_gid)),
                 space_span.clone(),
-                Span::from(get_size_str(file.size)),
+                Span::from(get_size_str(size)),
                 space_span.clone(),
-                Span::from(file.path.clone()),
+                Span::from(item.name.clone()),
             ])));
         }
     }
