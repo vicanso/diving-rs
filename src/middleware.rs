32b1d3c446d900b4222d9b3a606ad9b5a2308e23
vicanso <tree.xie@outlook.com>
2023-06-22 10:46:35 +0800

    refactor: ingore ping access log

--- src/middleware.rs
+++ src/middleware.rs
@@ -12,19 +12,22 @@ pub async fn access_log<B>(
     next: Next<B>,
 ) -> Response {
     let started_at = Utc::now().timestamp_millis();
+    let path = req.uri().path().to_string();
     let uri = req.uri().to_string();
     let method = req.method().to_string();
     let resp = next.run(req).await;
-    let status = resp.status().as_u16();
-    let cost = Utc::now().timestamp_millis() - started_at;
-    tl_info!(
-        category = "accessLog",
-        ip = ip.to_string(),
-        method,
-        uri,
-        status,
-        cost,
-    );
+    if path != "/ping" {
+        let status = resp.status().as_u16();
+        let cost = Utc::now().timestamp_millis() - started_at;
+        tl_info!(
+            category = "accessLog",
+            ip = ip.to_string(),
+            method,
+            uri,
+            status,
+            cost,
+        );
+    }
     resp
 }
 
