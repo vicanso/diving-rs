9a2c37adf9abeda484bec67998731d2513ec2fd1
vicanso <tree.xie@outlook.com>
2023-08-16 20:42:35 +0800

    refactor: adjust web build base path

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
     chunkSizeWarningLimit: 1024 * 1024,
     rollupOptions: {
